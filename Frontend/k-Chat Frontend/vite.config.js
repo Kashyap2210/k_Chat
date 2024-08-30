2b97edcf20e06b0d3792ff334aaa999a4742733a
Kashyap 2210 <kash.cdac@gmail.com>
2024-08-30 19:08:01 +0530

    Changes to vite config file

--- Frontend/k-Chat Frontend/vite.config.js
+++ Frontend/k-Chat Frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/k-Chat/", // <-- Update with your repository name
+  base: "/k_Chat/", // <-- Update with your repository name
   plugins: [react()],
 });
